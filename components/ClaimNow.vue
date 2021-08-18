25797ac26310954e4f5eb6ddc27624cd3a29b7f4
Hideo Hattori <hhatto.jp@gmail.com>
2021-08-18 23:20:07 +0900

    add property list

--- components/ClaimNow.vue
+++ components/ClaimNow.vue
@@ -49,7 +49,17 @@
 
     <section v-if="claimUrl" class="then">
       <div class="stakes-social">
-        <h2>Next?</h2>
+        <h2>
+          Stake the DEV you receive to OSS project Staking will help developers
+          and increase your DEV
+        </h2>
+        <div class="stakes-social-property-card-list">
+          <PropertyCard
+            v-for="(property, idx) in properties"
+            :key="idx"
+            :property="property"
+          />
+        </div>
         <img
           width="100%"
           src="https://devprotocol.xyz/assets/img/macbook_stakes.png"
@@ -103,6 +113,160 @@
 import { mapActions, mapState } from 'vuex'
 
 export default {
+  data() {
+    return {
+      properties: [
+        {
+          name: 'reduxjs/redux-toolkit',
+          address: '0xB96cBb406F8Eb2712eD8A9055e285E28fd076548',
+          imageUrl:
+            'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/0xB96cBb406F8Eb2712eD8A9055e285E28fd076548/cover_image/rtk_header_d6ace5a2a7.png',
+          description:
+            'The official, opinionated, batteries-included toolset for efficient Redux development',
+          author: {
+            name: 'Redux Toolkit',
+            imageUrl:
+              'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/0x29013Dc5d2Ee52B7ace7FEaD33C19A5903b5451e/redux_Logo_white_centered_ca69f7be41.png',
+          },
+        },
+        {
+          name: 'nolimits4web/swiper',
+          address: '0x65D6c1a5A31d49591d3BE41D1cc9a91F2b6209f4',
+          imageUrl:
+            'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/share_banner_5961043ec8.png',
+          description:
+            'Swiper is the most modern free mobile touch slider with hardware accelerated transitions and amazing native behavior. It is intended to be used in mobile websites, mobile web apps, and mobile native/hybrid apps.',
+          author: {
+            name: 'nolimits4web',
+            imageUrl:
+              'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/999588_e4bbaa7512.jpeg',
+          },
+        },
+        {
+          name: 'LunarVim/LunarVim',
+          address: '0xe014A52354136B678ff5030397224179bD0F9657',
+          imageUrl:
+            'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/0xe014A52354136B678ff5030397224179bD0F9657/cover_image/lunarvim_logo_light_b7fda576ce.png',
+          description: 'LunarVim - fast extensible IDE Layer for Neovim',
+          author: {
+            name: 'chris@machine',
+            imageUrl:
+              'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/0xc763a82f81A0C5D525b1Af74A23E996751735307/me2_3a8650af87.jpg',
+          },
+        },
+        {
+          name: 'WalletConnect/walletconnect-monorepo',
+          address: '0x4cb307CF044f408C8e71caaf36d8De1Ea5ebA0BC',
+          imageUrl:
+            'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/1500x500_0c32ff15aa',
+          description: 'Open protocol for connecting Wallets to Dapps',
+          author: {
+            name: 'WalletConnect',
+            imageUrl: 'https://asset.stakes.social/logo/dev.svg',
+          },
+        },
+        {
+          name: 'vyperlang/vyper',
+          address: '0x90168f9Ba07B95f629f8b77F789d46A549d6470F',
+          imageUrl:
+            'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/d3e6c900_6a1b_11ea_91be_d87dfda8b8e7_36d483d5b7.jpg',
+          description:
+            'Vyper is a contract-oriented, pythonic programming language that targets the Ethereum Virtual Machine (EVM).',
+          author: {
+            name: 'Vyper',
+            imageUrl:
+              'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/d3e6c900_6a1b_11ea_91be_d87dfda8b8e7_36d483d5b7.jpg',
+          },
+        },
+        {
+          name: 'talentlessguy/tinyhttp',
+          address: '0x14308514785B216904a41aB817282d25425Cce39',
+          imageUrl:
+            'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/0x14308514785B216904a41aB817282d25425Cce39/cover_image/cover_52d54ababd.jpg',
+          description:
+            '🎁 EACH ADDRESS THAT STAKES 1K+ DEV GETS A 10DEV BONUS 🎁 tinyhttp is a modern Express-like web framework written in TypeScript and compiled to native ESM, that uses a bare minimum amount of dependencies trying to avoid legacy hell. - ⚡ 2x faster than Express - ⚙ Express middleware compat - ↪ Async middleware support - ☑ Native ESM and CommonJS support - 🚀 No legacy dependencies, just the JavaScript itself - 🔨 Types out of the box - 🔥 Prebuilt middleware for modern Node.js ',
+          author: {
+            name: 'v1rtl',
+            imageUrl:
+              'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/0xD3B282e9880cDcB1142830731cD83f7ac0e1043f/pfp_82728c3db8.jpg',
+          },
+        },
+        {
+          name: 'lukechilds/keyv',
+          address: '0x4Dfd87f4D9cb1f5345AE3F5fAB85be4218543283',
+          imageUrl: 'https://asset.stakes.social/logo/dev.svg',
+          description:
+            'Simple key-value storage with support for multiple backends. Keyv provides a consistent interface for key-value storage across multiple backends via storage adapters. It supports TTL based expiry, making it suitable as a cache or a persistent key-value store.',
+          author: {
+            name: '',
+            imageUrl: 'https://asset.stakes.social/logo/dev.svg',
+          },
+        },
+        {
+          name: 'framework7io',
+          address: '0x4E451B79800CCCC76a194A908cD06487918f4701',
+          imageUrl:
+            'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/0x4E451B79800CCCC76a194A908cD06487918f4701/cover_image/share_banner_bd412ce956.jpg',
+          description:
+            'Framework7 - is a free and open source framework to develop mobile, desktop or web apps with native look and feel',
+          author: {
+            name: 'nolimits4web',
+            imageUrl:
+              'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/999588_e4bbaa7512.jpeg',
+          },
+        },
+        {
+          name: 'vadimdemedes/ink',
+          address: '0x7dC42FA19C5CfDfACdcf7B7bEeE9b6CfC77A6F1b',
+          imageUrl:
+            'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/dd18698b_66fa74abb6',
+          description:
+            '🌈 React for interactive command-line apps. Ink provides the same component-based UI building experience that React offers in the browser, but for command-line apps.',
+          author: {
+            name: 'Vadim Demedes',
+            imageUrl:
+              'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/66f211dc_fbdb1a7402',
+          },
+        },
+        {
+          name: 'crowi/crowi',
+          address: '0x375bB44E669A14a09a982b98B74c677a8400EDbf',
+          imageUrl:
+            'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/crowi_logo_master_inverse_for_dev_88b00401a4.png',
+          description: 'Empower the team with sharing your knowledge.',
+          author: {
+            name: 'sotarok',
+            imageUrl:
+              'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/0x4908Bf2c81a71647e6a579acbE1Bf1aa2778c874/sotarok_icon2_square_c5257a1951.png',
+          },
+        },
+        {
+          name: 'hhatto/autopep8',
+          address: '0x6FC83305afb1083CE1Db511d252B47A38CFCE1f7',
+          imageUrl: 'https://asset.stakes.social/logo/dev.svg',
+          description:
+            'A tool that automatically formats Python code to conform to the PEP 8 style guide.',
+          author: {
+            name: 'hhatto',
+            imageUrl:
+              'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/0x006f7DA5fE8B1a45D364a677499333099CDb4b75/face_b3bec3b50c.jpg',
+          },
+        },
+        {
+          name: 'netbootxyz/netboot.xyz',
+          address: '0xdFB462BfD71128b96DB742a2621aEb801c4FD611',
+          imageUrl:
+            'https://storageaccountdevfobc15.blob.core.windows.net/dev-for-apps-images/assets/0xdFB462BfD71128b96DB742a2621aEb801c4FD611/cover_image/dbe68b80_7b20_11e9_8541_8d2b4dd8f636_8f38bf767b.png',
+          description:
+            'Network bootable operating system installer based on iPXE',
+          author: {
+            name: 'netboot.xyz',
+            imageUrl: 'https://asset.stakes.social/logo/dev.svg',
+          },
+        },
+      ],
+    }
+  },
   async fetch() {
     await this.initDevInfo()
   },
@@ -175,13 +339,22 @@ export default {
   h2 {
     font-family: 'Whyte Inktrap', sans-serif;
     font-weight: bold;
-    font-size: 6rem;
+    font-size: 2.8rem;
     @media (max-width: 576px) {
       font-size: 4rem;
     }
   }
   .ss-explore-button {
     margin-top: 4rem;
+  }
+
+  &-property-card-list {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(345px, 1fr));
+    gap: 6rem 1rem;
+    -moz-box-pack: justify;
+    margin: 1rem 0;
+    justify-content: space-between;
   }
 }
 
---
+++ components/PropertyCard.vue
@@ -0,0 +1,130 @@
+<template>
+  <div>
+    <div class="property-card">
+      <div
+        class="property-card-cover-image"
+        :style="{ 'background-image': 'url(' + property.imageUrl + ')' }"
+      />
+      <a
+        :href="'https://stakes.social/' + property.address"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div class="property-card-content">
+          <div class="property-card-title">{{ property.name }}</div>
+          <span class="property-card-description">
+            {{ property.description }}
+          </span>
+          <div class="property-card-flex-row">
+            <div class="property-card-avatar-image">
+              <img :src="property.author.imageUrl" />
+            </div>
+            <div class="property-card-flex-column">
+              <span>Creator</span>
+              <span :style="{ color: '#1AC9FC' }">{{
+                property.author.name
+              }}</span>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    property: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+  },
+  fetch() {
+    console.log(this.property)
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.property-card {
+  grid-template-rows: auto 1fr auto;
+  grid-template-columns: 1fr;
+  justify-content: stretch;
+  border: solid 1px rgba(0, 0, 0, 0.1);
+  border-bottom: 0;
+  border-radius: 6px;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  cursor: pointer;
+  pointer-events: auto;
+  background: #fff;
+  overflow: hidden;
+  height: 350px;
+  width: 350px;
+
+  &-cover-image {
+    position: relative;
+    background-size: cover;
+    background-position: center;
+    padding-top: 20%;
+  }
+
+  &-content {
+    display: grid;
+    grid-auto-flow: row;
+    grid-gap: 0.7rem;
+    align-content: baseline;
+    text-align: left;
+    grid-template-rows: auto 2fr 1fr;
+    padding: 4px 16px 0 16px;
+  }
+
+  &-title {
+    font-size: 1.4em;
+    font-weight: 400;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+  &-description {
+    color: grey;
+    margin: 0;
+    flex-grow: 1;
+    font-size: 0.95em;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 5; /* number of lines to show */
+    -webkit-box-orient: vertical;
+  }
+  &-flex-row {
+    display: flex;
+    img {
+      border-radius: 90px;
+    }
+  }
+  &-flex-column {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 0;
+    max-lines: 3;
+    padding-bottom: 16px;
+    span {
+      margin-left: 10px;
+      font-size: 0.9em;
+    }
+  }
+  &-avatar-image {
+    img {
+      border: 1px solid lightgrey;
+      border-radius: 90px;
+      object-fit: cover;
+      object-position: center;
+      width: 60px;
+      height: 60px;
+    }
+  }
+}
+</style>
