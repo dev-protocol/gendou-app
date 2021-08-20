9ba3a91bc59293337a9ee669342185f911af031c
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2021-08-20 15:17:46 +0900

    Modified text

--- components/ClaimNow.vue
+++ components/ClaimNow.vue
@@ -11,7 +11,7 @@
       </div>
 
       <div v-if="claimUrl !== ''" class="warning">
-        Rewards will be picked up by October 31th.<br />
+        Please claim by October 31st<br />
         You will not be able to receive it after the period
       </div>
 
